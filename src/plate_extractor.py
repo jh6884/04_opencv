f1c6366aca5ac05af9bdc7a9378c02e51bcd01ed
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-07-31 12:07:31 +0900

    코드 수정
    클릭시 생기는 점이 너무 큼, 크기 수정
    변환 후 이미지의 크기는 고정이므로 폭, 높이 계산 과정 필요없음, 해당 코드 삭제
    이미지 저장 알고리즘을 기준에 맞게 변경, 결과 이미지의 출력 타이밍을 변경

--- src/plate_extractor.py
+++ src/plate_extractor.py
@@ -2,10 +2,12 @@
 
 import cv2
 import numpy as np
+import os
 
-win_name = "scanning"
-img_name = 'car_01'
-img = cv2.imread('../img/'+img_name+'.jpg')
+img_name = 'car_02' # 이미지명
+
+win_name = "License Plate Extractor"
+img = cv2.imread(f'../img/{img_name}.jpg')
 rows, cols = img.shape[:2]
 draw = img.copy() # 점을 그릴 이미지 사본
 pts_cnt = 0 # 수집한 좌표의 개수
@@ -14,13 +16,14 @@ pts = np.zeros((4,2), dtype=np.float32)
 def onMouse(event, x, y, flags, param): # 마우스 이벤트 콜백 함수
     global pts_cnt
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(draw, (x,y), 10, (0, 255, 0), -1) # 클릭한 곳에 동그라미 표시
+        cv2.circle(draw, (x,y), 5, (0, 255, 0), -1) # 클릭한 곳에 동그라미 표시
         cv2.imshow(win_name, draw)
 
-        pts[pts_cnt] = [x,y] # 좌표 저장
+        pts[pts_cnt] = [x,y] # 좌표 배열에 저장
         pts_cnt += 1 # 수집한 좌표 개수에 +1
 
         if pts_cnt == 4: # 좌표 4개 수집 시
+            cv2.destroyWindow(win_name)
             sm = pts.sum(axis=1) # 좌표 각각 x+y 계산
             diff = np.diff(pts, axis = 1) # 좌표 각각 x-y 계산
 
@@ -32,13 +35,9 @@ def onMouse(event, x, y, flags, param): # 마우스 이벤트 콜백 함수
             # 변환 전 4개 좌표
             pts1 = np.float32([topLeft, topRight, bottomRight, bottomLeft])
 
-            # 변환 후 영상에 사용할 서류의 폭과 높이 계산
-            w1 = abs(bottomRight[0] - bottomLeft[0]) # 하단 좌우 좌표간의 거리
-            w2 = abs(topRight[0] - topLeft[0]) # 상단 좌우 좌표간의 거리
-            h1 = abs(topRight[1] - bottomRight[1]) # 우측 상하 거리
-            h2 = abs(topLeft[1] - bottomLeft[1]) # 좌측 상하 거리
-            width = max([w1, w2])
-            height = max([h1, h2])
+            # 변환 후 이미지의 크기 고정
+            width = 300
+            height = 150
 
             # 변환 후 4개 좌표
             pts2 = np.float32([[0,0], [width-1, 0], [width-1, height-1], [0, height-1]])
@@ -48,10 +47,17 @@ def onMouse(event, x, y, flags, param): # 마우스 이벤트 콜백 함수
 
             # 원근 변환 적용
             result = cv2.warpPerspective(img, mtrx, (int(width), int(height)))
-            cv2.imshow('scanned', result)
 
             # 변환된 이미지 저장
-            cv2.imwrite(result, '../extracted_plates/'+img_name+'.jpg')
+            existing_files = len(os.listdir('../extracted_plates'))
+            file_name = f'../extracted_plates/plate_{existing_files+1:02d}.jpg'
+            save = cv2.imwrite(file_name, result)
+            if save:
+                print('File saved! Press any key to exit program')
+                cv2.imshow('Extracted Plate', result)
+            else:
+                print('Save failed')
+
 
 cv2.imshow(win_name, img)
 cv2.setMouseCallback(win_name, onMouse)
